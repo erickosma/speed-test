8e75129f1b1e38c32e46ae5280284cfae1b73939
erickosma <erickosma@gmail.com>
2019-11-03 19:25:21 -0300

    google speed-test

--- run.py
+++ run.py
@@ -4,26 +4,46 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pprint
 import datetime
+import csv
 
 servers = []
 pp = pprint.PrettyPrinter()
 now = datetime.datetime.now()
 
+def insert_error():
+    tableName = "error.csv"
+    row = [datetime.datetime.now()]
+    with open(tableName, 'a') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerow(row)
+    csvFile.close()
+    exit()
+
+
+def get_speed():
+    global results
+    s = speedtest.Speedtest()
+    s.get_servers(servers)
+    s.get_best_server()
+    s.download()
+    s.upload()
+    s.results.share()
+    return s.results
+
+
+try:
+    results = get_speed()
+except:
+    insert_error()
+
+
+
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('Project-a9c411992d1a.json', scope)
 client = gspread.authorize(creds)
-
-s = speedtest.Speedtest()
-s.get_servers(servers)
-s.get_best_server()
-s.download()
-s.upload()
-s.results.share()
-
-results = s.results
-# results_dict = results.dict()
-
 sheet = client.open_by_key('1NCDOdFtQrJyf-nT4sJijvnoHkZiIG7C8BJPkYPqjdRc').sheet1
+
+# results_dict = results.dict()
 # insert
 row = [now.strftime("%Y-%m-%d %X"),
        round(results.ping, 2),
