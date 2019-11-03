364e23c3720f4a5b86cc25b8ac0771fa0c956e52
erickosma <erickosma@gmail.com>
2019-11-03 15:26:15 -0300

    speed test

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
-print("teste")
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import speedtest
+
+servers = []
+
+s = speedtest.Speedtest()
+s.get_servers(servers)
+s.get_best_server()
+s.download()
+s.upload()
+s.results.share()
+
+results = s.results
+# results_dict = results.dict()
+
+print(results.ping)
+print((results.download / 1000.0 / 1000.0))
+print((results.upload / 1000.0 / 1000.0))
